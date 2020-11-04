6e45cf871617ae21b2343154821204f39570ca5a
Sam Kaplan <sam.kaplan@chevron.com>
2020-11-03 21:17:18 -0600

    Fix unit tests for additional retry code

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,10 +20,10 @@ sleep(60)
     y = unsafe_wrap(Array, x, (2,); own=false)
     @test y == [500,503]
 
-    @test unsafe_load(cglobal((:N_CURL_RETRY_CODES, AzStorage.libAzStorage), Cint)) == 3
+    @test unsafe_load(cglobal((:N_CURL_RETRY_CODES, AzStorage.libAzStorage), Cint)) == 4
     x = unsafe_load(cglobal((:CURL_RETRY_CODES, AzStorage.libAzStorage), Ptr{Clong}))
-    y = unsafe_wrap(Array, x, (3,); own=false)
-    @test y == [7,55,56]
+    y = unsafe_wrap(Array, x, (4,); own=false)
+    @test y == [7,28,55,56]
 end
 
 @testset "Containers, list" begin
