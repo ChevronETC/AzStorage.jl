9c9315a37752a231e746812909814a36e25e87d3
samkaplan <sam.kaplan@chevron.com>
2022-05-10 18:06:23 +0000

    retry on curl ssl handshake error

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,10 +20,10 @@ sleep(60)
     y = unsafe_wrap(Array, x, (2,); own=false)
     @test y == [500,503]
 
-    @test unsafe_load(cglobal((:N_CURL_RETRY_CODES, AzStorage.libAzStorage), Cint)) == 5
+    @test unsafe_load(cglobal((:N_CURL_RETRY_CODES, AzStorage.libAzStorage), Cint)) == 6
     x = Sys.iswindows() ? unsafe_load(cglobal((:CURL_RETRY_CODES, AzStorage.libAzStorage), Ptr{Clonglong})) : unsafe_load(cglobal((:CURL_RETRY_CODES, AzStorage.libAzStorage), Ptr{Clong}))
-    y = unsafe_wrap(Array, x, (5,); own=false)
-    @test y == [6,7,28,55,56]
+    y = unsafe_wrap(Array, x, (6,); own=false)
+    @test y == [6,7,28,35,55,56]
 end
 
 @testset "Containers, equivalent" begin
